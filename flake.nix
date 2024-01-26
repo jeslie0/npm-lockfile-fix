e9851274afa12b04d98e694ed089aa9cde8d7349
James Leslie <jamesleslie@posteo.net>
2024-01-26 21:23:33 +0000

    Remove dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -2,47 +2,75 @@
   description = "A very basic python flake template, providing a devshell.";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pyPkgs = pkgs.python310Packages;
-        customPython = pkgs.python39.buildEnv.override {
-          extraLibs = with pyPkgs; [
-            ipython
-            setuptools
-            requests
-          ];
-        };
-        packageName = with builtins; head (match "^.*name[[:space:]]*=[[:space:]][\"]([^[:space:]]*)[\"][,].*$" (readFile ./setup.py));
-        version = with builtins; head (match "^.*version[[:space:]]*=[[:space:]][\"]([^[:space:]]*)[\"][,].*$" (readFile ./setup.py));
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      forAllSystems =
+        nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor =
+        forAllSystems (system:
+          import nixpkgs {
+            inherit system;
+          }
+        );
+
+      pyPkgs = system:
+        nixpkgsFor.${system}.python310Packages;
+
+      packageName = with builtins;
+        head (match "^.*name[[:space:]]*=[[:space:]][\"]([^[:space:]]*)[\"][,].*$" (readFile ./setup.py));
+
+      version = with builtins;
+        head (match "^.*version[[:space:]]*=[[:space:]][\"]([^[:space:]]*)[\"][,].*$" (readFile ./setup.py));
+
       in
       {
+        packages =
+          forAllSystems (system:
+            let
+              pkgs =
+                nixpkgsFor.${system};
 
-        packages.default = pyPkgs.buildPythonPackage {
-          pname = packageName;
-          inherit version;
-          src = ./.;
-          doCheck = false;
+            in
+              {
+                default = (pyPkgs system).buildPythonPackage {
+                  pname = packageName;
+                  inherit version;
+                  src = ./.;
+                  doCheck = false;
 
-          propagatedBuildInputs = [ pyPkgs.requests ];
-          meta = {
-            homepage = "https://github.com/jeslie0/npm-lockfile-fix";
-            description = "";
-            license = pkgs.lib.licenses.mit;
-          };
-        };
+                  propagatedBuildInputs = [ (pyPkgs system).requests ];
+                  meta = {
+                    homepage = "https://github.com/jeslie0/npm-lockfile-fix";
+                    description = "";
+                    license = pkgs.lib.licenses.mit;
+                  };
+                };
+              }
+          );
 
-        devShell = pkgs.mkShell {
-          packages = with pkgs;
-            [ python310Packages.python-lsp-server ];
-          inputsFrom = [ self.packages.${system}.default ];
-        };
-      }
-    );
+        devShells =
+          forAllSystems (system:
+            let
+              pkgs =
+                nixpkgsFor.${system};
+            in
+              {
+                default =
+                  pkgs.mkShell {
+                    packages = with pkgs;
+                      [ python310Packages.python-lsp-server ];
+
+                    inputsFrom =
+                      [ self.packages.${system}.default ];
+                  };
+              }
+          );
+      };
 }
